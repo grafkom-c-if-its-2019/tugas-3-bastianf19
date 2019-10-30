7968ade43f9e704e54891ffef2a172493a38f947
bastianf19 <bastian.farandy@gmail.com>
2019-10-30 18:03:35 +0700

    update index.html + index js

--- index.js
+++ index.js
@@ -5,127 +5,45 @@
   function main() {
     var canvas = document.getElementById("glcanvas");
     var gl = glUtils.checkWebGL(canvas);
-    // B Non-Fill
+    // vertex B
     var vertexShader = glUtils.getShader(gl, gl.VERTEX_SHADER, glUtils.SL.Shaders.v1.vertex);
-    var fragmentShader = glUtils.getShader(gl, gl.FRAGMENT_SHADER, glUtils.SL.Shaders.v1.fragment);
+    var fragmentShader = glUtils.getShader(gl, gl.FRAGMENT_SHADER, glUtils.SL.Shaders.v3.fragment);
     var program = glUtils.createProgram(gl, vertexShader, fragmentShader);
-    // B Fill
+    // vertex kubus
     var vertexShader2 = glUtils.getShader(gl, gl.VERTEX_SHADER, glUtils.SL.Shaders.v2.vertex);
     var program2 = glUtils.createProgram(gl, vertexShader2, fragmentShader);
-    // B Non-Fill
-    function DrawBNoFill(){
-      // tambah 0.3
-      var line1 = [
-        // x, y,       r, g, b
-        -0.5, 0.5, 0.7, 0.0, 1.0,
-        -0.5, -0.5, 0.1, 1.0, 0.6
-      ];
-      var line2 = [
-        // x, y,        r, g, b
-        -0.5, 0.5, 0.7, 0.0, 1.0,
-        -0.35, 0.5, 0.1, 1.0, 0.6
-      ];
-      var line3 = [
-        // x, y,        r, g, b
-        -0.45, 0.1, 0.7, 0.0, 1.0,
-        -0.35, 0.1, 0.1, 1.0, 0.6
-      ];
-      var line4 = [
-        // x, y,        r, g, b
-        -0.5, -0.5, 0.7, 0.0, 1.0,
-        -0.35, -0.5, 0.1, 1.0, 0.6
-      ];
-      //Garis Inner
-      var line5 = [
-        // x, y,        r, g, b
-        -0.45, 0.2, 0.7, 0.0, 1.0,
-        -0.35, 0.2, 0.1, 1.0, 0.6
-      ];
-      var line6 = [
-        // x, y,        r, g, b
-        -0.45, 0.4, 0.7, 0.0, 1.0,
-        -0.35, 0.4, 0.1, 1.0, 0.6
-      ];
-      var line7 = [
-        // x, y,        r, g, b
-        -0.45, 0.4, 0.7, 0.0, 1.0,
-        -0.45, 0.2, 0.1, 1.0, 0.6
-      ];
-      var line8 = [
-        // x, y,        r, g, b
-        -0.45, -0.0001, 0.7, 0.0, 1.0,
-        -0.45, -0.4, 0.1, 1.0, 0.6
-      ];
-      var line9 = [
-        // x, y,        r, g, b
-        -0.45, -0.0001, 0.7, 0.0, 1.0,
-        -0.35, -0.0001, 0.1, 1.0, 0.6
-      ];
-      var line10 = [
-        // x, y,        r, g, b
-        -0.45, -0.4, 0.7, 0.0, 1.0,
-        -0.35, -0.4, 0.1, 1.0, 0.6
-      ];
-      var cir2 = [],
-        cir3 = [],
-        cir4 = [],
-        cir5 = [];
-      // B atas 1
-      for (var k = 0.0; k <= 180; k += 1) {
-        var l = k * Math.PI / 180;
-  
-        var ling2 = [
-          -0.35 + Math.sin(l) * 0.15,
-          0.3 + Math.cos(l) * 0.2, 0.7, 0.0, 1.0,
-        ];
-        cir2 = cir2.concat(ling2);
-      }
-      // B atas 2
-      for (var a = 0.0; a <= 180; a += 1) {
-        var b = a * Math.PI / 180;
-  
-        var ling4 = [
-          -0.35 + Math.sin(b) * 0.1,
-          0.3 + Math.cos(b) * 0.1, 0.7, 0.0, 1.0,
-        ];
-        cir4 = cir4.concat(ling4);
-      }
-      // B bawah 1
-      for (var m = 0.0; m <= 180; m += 1) {
-        var n = m * Math.PI / 180;
-  
-        var ling3 = [
-          -0.35 + Math.sin(n) * 0.25,
-          -0.2 + Math.cos(n) * 0.3, 0.7, 0.0, 1.0,
-        ];
-        cir3 = cir3.concat(ling3);
-      }
-      // B bawah 2
-      for (var c = 0.0; c <= 180; c += 1) {
-        var d = c * Math.PI / 180;
-  
-        var ling5 = [
-          -0.35 + Math.sin(d) * 0.2,
-          -0.20 + Math.cos(d) * 0.20, 0.7, 0.0, 1.0,
-        ];
-        cir5 = cir5.concat(ling5);
-      }
-    // Draw B Non-Fill
-    DrawBuffer(gl.LINES, line1, 2, program);
-    DrawBuffer(gl.LINES, line2, 2, program);
-    DrawBuffer(gl.LINES, line3, 2, program);
-    DrawBuffer(gl.LINES, line4, 2, program);
-    DrawBuffer(gl.LINES, line5, 2, program);
-    DrawBuffer(gl.LINES, line6, 2, program);
-    DrawBuffer(gl.LINES, line7, 2, program);
-    DrawBuffer(gl.LINES, line8, 2, program);
-    DrawBuffer(gl.LINES, line9, 2, program);
-    DrawBuffer(gl.LINES, line10, 2, program);
-    DrawBuffer(gl.LINE_STRIP, cir2, 181, program);
-    DrawBuffer(gl.LINE_STRIP, cir3, 181, program);
-    DrawBuffer(gl.LINE_STRIP, cir4, 181, program);
-    DrawBuffer(gl.LINE_STRIP, cir5, 181, program);
-  }
+    
+    var kubus = ([
+      //BAWAH
+      -0.3, -0.8, 0.7, 255, 255, 255,
+      0.4, -0.8, 0.7, 255, 255, 255,
+      0.4, -0.8, 0.7, 255, 255, 255,
+      0.4, -0.8, -0.6, 255, 255, 255,
+      0.4, -0.8, -0.6, 255, 255, 255,
+      -0.3, -0.8, -0.6, 255, 255, 255,
+      -0.3, -0.8, -0.6, 255, 255, 255,
+      -0.3, -0.8, 0.7, 255, 255, 255,
+      //ATAS
+      -0.3, 0.6, 0.7, 255, 255, 255,
+      0.4, 0.6, 0.7, 255, 255, 255,
+      0.4, 0.6, 0.7, 255, 255, 255,
+      0.4, 0.6, -0.6, 255, 255, 255,
+      0.4, 0.6, -0.6, 255, 255, 255,
+      -0.3, 0.6, -0.6, 255, 255, 255,
+      -0.3, 0.6, -0.6, 255, 255, 255,
+      -0.3, 0.6, 0.7, 255, 255, 255,
+      //BELAKANG
+      -0.3, -0.8, 0.7, 255, 255, 255,
+      -0.3, 0.6, 0.7, 255, 255, 255,
+      0.4, -0.8, 0.7, 255, 255, 255,
+      0.4, 0.6, 0.7, 255, 255, 255,
+      //DEPAN
+      0.4, -0.8, -0.6, 255, 255, 255,
+      0.4, 0.6, -0.6, 255, 255, 255,
+      -0.3, -0.8, -0.6, 255, 255, 255,
+      -0.3, 0.6, -0.6, 255, 255, 255
+    ]);  
+    
     // B Fill
     // +0.4
   function DrawBFill() {
@@ -227,35 +145,81 @@
       gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verArray), gl.STATIC_DRAW);
       gl.drawArrays(type, 0, n);
     }
-    var thetaLocation = gl.getUniformLocation(program, 'theta');
-    var theta = 0.0;
-    var scaleXLocation = gl.getUniformLocation(program2, 'scaleX');
-    // var scaleYLocation = gl.getUniformLocation(program2, 'scaleY');
-    var scaleX = 1.0;
-    // var scaleY = 1.0;
-    var melebar = 1.0;
+    function DrawBufferKubus(type, verArray, n, noprogram) {
+      var triangleVertexBufferObject = gl.createBuffer();
+      gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
+
+      var vPosition = gl.getAttribLocation(noprogram, 'vPosition');
+      var vColor = gl.getAttribLocation(noprogram, 'vColor');
+
+      gl.vertexAttribPointer(
+        vPosition,  // variabel yang memegang posisi attribute di shader
+        3,          // jumlah elemen per atribut
+        gl.FLOAT,   // tipe data atribut
+        gl.FALSE,
+        6 * Float32Array.BYTES_PER_ELEMENT, // ukuran byte tiap vertex 
+        0                                   // offset dari posisi elemen di array
+      );
+      gl.vertexAttribPointer(
+        vColor,
+        3,
+        gl.FLOAT,
+        gl.FALSE,
+        6 * Float32Array.BYTES_PER_ELEMENT,
+        3 * Float32Array.BYTES_PER_ELEMENT
+      );
+      gl.enableVertexAttribArray(vPosition);
+      gl.enableVertexAttribArray(vColor);
+
+      var vPosition = gl.getAttribLocation(noprogram, 'vPosition');
+      var vColor = gl.getAttribLocation(noprogram, 'vColor');
+      gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verArray), gl.STATIC_DRAW);
+      gl.drawArrays(type, 0, n);
+    }
+    var thetaLocation1 = gl.getUniformLocation(program, 'theta1');
+    var transLocation1 = gl.getUniformLocation(program, 'trans1');
+    var theta = [10, 20, 0];
+    var trans1 = [0, 0, 0];
+    var X1 = 0.0080;
+    var Y1 = 0.0090;
+    var Z1 = 0.0130;
+
     function render() {
       // Bersihkan layar jadi hitam
       gl.useProgram(program);
       gl.clearColor(0.0, 0.0, 0.0, 1.0);
       // Bersihkan buffernya canvas
       gl.clear(gl.COLOR_BUFFER_BIT);
-      // B Non-Fill
-      theta += 0.0190;
-      gl.uniform1f(thetaLocation, theta);
-      DrawBNoFill();
+
+      if (trans1[0] >= 0.4 * 0.8 || trans1[0] <= -0.3 * 0.8) {
+        X1 *= -1;
+      }
+      trans1[0] += X1;
+
+      if (trans1[1] >= 0.6 * 0.8 || trans1[1] <= -0.8 * 0.8) {
+        Y1 *= -1;
+      }
+      trans1[1] += Y1;
+
+      if (trans1[2] >= 0.7 * 0.8 || trans1[2] <= -0.6 * 0.8) {
+        Z1 *= -1;
+      }
+      trans1[2] += Z1;
+
+      gl.uniform3fv(transLoc1, trans1);
+      thetaA1[1] += 0.190;
+      gl.uniform3fv(thetaLoc1, thetaA1); 
+      DrawBFill(); 
       requestAnimationFrame(render);
     }
     resizer();
     render();
-    // B Fill
+    // kubus
     function render2() {
-      gl.useProgram(program2);
-      if (scaleX >= 1) melebar = -1;
-      else if (scaleX <= -1) melebar = 1;
-      scaleX += 0.0190 * melebar;
-      gl.uniform1f(scaleXLocation, scaleX);
-      DrawBFill();
+      gl.useProgram(program2); 
+      var thetaCube = [10, 10, 0];
+      gl.uniform3fv(thetaLocCube, thetaCube);
+      drawShapes3(gl.LINES, kubus, 24);
       requestAnimationFrame(render2);
     }
     render2();
